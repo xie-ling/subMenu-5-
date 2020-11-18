4899cbf923c839bda42de53ca6e6aa2e297ec196
xie <1259702471@qq,com>
2020-11-18 13:01:58 +0800

    Initial Commit

---
+++ pages/jd-index/jd-index.js
@@ -0,0 +1,114 @@
+// pages/jd-index/jd-index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    autoplay:true,
+    interval:3000,
+    imgs:[
+      "/images/haibao/haibao-1.png",
+      "/images/haibao/haibao-2.png"
+    ],
+    element:[
+      {
+        image:"/images/index/1.png",
+        name:"领优惠劵"
+    },
+    {
+      image:"/images/index/2.png",
+      name:"9.9元拼"
+    },
+    {
+      image:"/images/index/3.png",
+      name:"找折扣"
+    },
+    {
+      image:"/images/index/4.png",
+      name:"闪购"
+    },
+    {
+      image:"/images/index/5.png",
+      name:"领京东"
+    },
+    {
+      image:"/images/index/6.png",
+      name:"打卡有奖"
+    },
+    {
+      image:"/images/index/7.png",
+      name:"京东服饰"
+    },
+    {
+      image:"/images/index/8.png",
+      name:"京东生鲜"
+    },
+    {
+      image:"/images/index/9.png",
+      name:"京东手机"
+    },
+    {
+      image:"/images/index/10.png",
+      name:"全部频道"
+    }
+
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
